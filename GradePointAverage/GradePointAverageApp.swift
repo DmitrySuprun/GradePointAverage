07d4768d386c3c8912a68dd72bc26fe99a5be12d
d.m.suprun <d.m.suprun@tinkoff.ru>
2024-03-20 20:41:37 +0300

    Initial Commit

---
+++ GradePointAverage/GradePointAverageApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradePointAverageApp.swift
+//  GradePointAverage
+//
+//  Created by Dmitriy Suprun on 20.03.24.
+//
+
+import SwiftUI
+
+@main
+struct GradePointAverageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
