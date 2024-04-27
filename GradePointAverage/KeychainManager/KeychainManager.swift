aafa7db7238496c9b138c90b765be4f216cae022
d.m.suprun <d.m.suprun@tinkoff.ru>
2024-04-27 10:41:40 +0300

    Add keychain manager

---
+++ GradePointAverage/KeychainManager/KeychainManager.swift
@@ -0,0 +1,58 @@
+//
+//  KeychainManager.swift
+//  GradePointAverage
+//
+//  Created by Dmitriy Suprun on 27.04.24.
+//
+
+import Security
+import Foundation
+
+private extension String {
+    static let keychainService = "com.gradepointaverage.service"
+}
+
+class KeychainManager {
+    
+    static func savePassword(account: String, password: String) -> Bool {
+        let service = String.keychainService
+        
+        guard let passwordData = password.data(using: .utf8) else { return false }
+        
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: account,
+            kSecValueData as String: passwordData
+        ]
+        
+        // Delete password if exist
+        SecItemDelete(query as CFDictionary)
+        
+        // Add new password
+        let status = SecItemAdd(query as CFDictionary, nil)
+        return status == noErr
+    }
+    
+    static func getPassword(account: String) -> String? {
+        let service = String.keychainService
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: account,
+            kSecReturnData as String: true,
+            kSecMatchLimit as String: kSecMatchLimitOne
+        ]
+        
+        var result: AnyObject?
+        let status = withUnsafeMutablePointer(to: &result) {
+            SecItemCopyMatching(query as CFDictionary, UnsafeMutablePointer($0))
+        }
+        
+        if status == noErr, let data = result as? Data, let password = String(data: data, encoding: .utf8) {
+            return password
+        } else {
+            return nil
+        }
+    }
+}
